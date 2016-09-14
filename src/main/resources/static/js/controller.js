3ec67213cc840adc9fbb357ce16d5a57e92f1805
Martin Sieber <mrtnsbr@gmail.com>
2016-09-14 15:34:09 +0200

    First actually working version. Ugly and a lot to do, but it works.

--- src/main/resources/static/js/controller.js
+++ src/main/resources/static/js/controller.js
@@ -1,4 +1,4 @@
-var app = angular.module('hh_app', ['ngResource']).controller("hh_view", function($scope) {
+var app = angular.module('hh_app', ['ngMaterial','ngResource']).controller("hh_view", function($scope) {
 
 	$scope.headers = {
 		values : [ "Map", "Hero", "Won" ]
@@ -7,9 +7,6 @@ var app = angular.module('hh_app', ['ngResource']).controller("hh_view", functio
 	$scope.add_button = "Add Person";
 	
 	$scope.stompConnected = false;
-	
-	$scope.matches = [];
-
 });
 
 app.factory(
@@ -18,7 +15,21 @@ app.factory(
 			return $resource("/hh/api/history/:id");
 		});
 
-app.controller('hh_controller', function($scope, $log, hhHistory) {
+app.factory(
+		"hhBestMatch",
+		function($resource, $log) {
+			return $resource("/hh/api/bestmatch/:name");
+		});
+
+app.controller('hh_controller', function($scope, $log, hhHistory, hhBestMatch) {
+	
+	$scope.stompClient = null;
+	
+	$scope.matches1 = [];
+	$scope.matches2 = [];
+	$scope.matches3 = [];
+	$scope.matches4 = [];
+	$scope.matches5 = [];
 
 	$scope.add = function() {
 		var firstname = 'A';
@@ -42,14 +53,12 @@ app.controller('hh_controller', function($scope, $log, hhHistory) {
 //
 //		private String date;
 		var person = {id: 4387231, map: "Test", hero: "Hero", lvl: "12"};
-		$log.info("Adding: " + person);
-		if ($scope.matches == null) {
-			$scope.matches = [];
-		}
-		$scope.matches.push(person);
+		$scope.matches1.push(person);
+		$scope.matches2.push(person);
+		$scope.matches3.push(person);
+		$scope.matches4.push(person);
+		$scope.matches5.push(person);
 	}
-
-	$scope.stompClient = null;
 
 	$scope.connect = function() {
 		$log.info("TEST");
@@ -62,6 +71,103 @@ app.controller('hh_controller', function($scope, $log, hhHistory) {
 				var update = JSON.parse(update.body)
 				$log.info('received: ' + update)
 				$log.info('received1: ' + update.enemies)
+				
+				$scope.enemyName1 = update.enemies[0];
+				$scope.enemyName2 = update.enemies[1];
+				$scope.enemyName3 = update.enemies[2];
+				$scope.enemyName4 = update.enemies[3];
+				$scope.enemyName5 = update.enemies[4];
+				
+				var bestMatch1 = hhBestMatch.get({name: $scope.enemyName1}, function(bestMatch1) {
+					$log.info("Match1: " + bestMatch1.id);
+					
+					$scope.enemyId1 = bestMatch1.id;
+					$scope.enemyMmr1 = bestMatch1.mmr;
+					$scope.enemyRegion1 = bestMatch1.region;
+					$scope.enemyGames1 = bestMatch1.numberOfGames;
+					$scope.enemyMatches1 = bestMatch1.numberOfMatches;
+					
+					if (!(angular.isUndefined($scope.enemyId1) || $scope.enemyId1 === null)) {
+						hhHistory.get({ id: $scope.enemyId1}, function(history) {
+							$log.info(history.rows);
+							$scope.matches1 = history.rows;
+						});
+					}
+				});
+				
+				var bestMatch2 = hhBestMatch.get({name: $scope.enemyName2}, function(bestMatch2) {
+					$log.info("Match2: " + bestMatch2.id);
+					
+					$scope.enemyId2 = bestMatch2.id;
+					$scope.enemyMmr2 = bestMatch2.mmr;
+					$scope.enemyRegion2 = bestMatch2.region;
+					$scope.enemyGames2 = bestMatch2.numberOfGames;
+					$scope.enemyMatches2 = bestMatch2.numberOfMatches;
+					
+					if (!(angular.isUndefined($scope.enemyId2) || $scope.enemyId2 === null)) {
+						hhHistory.get({ id: $scope.enemyId2}, function(history) {
+							$log.info(history.rows);
+							$scope.matches2 = history.rows;
+						});
+					}
+				});
+				
+				var bestMatch3 = hhBestMatch.get({name: $scope.enemyName3}, function(bestMatch3) {
+					$log.info("Match3: " + bestMatch3.id);
+					
+					$scope.enemyId3 = bestMatch3.id;
+					$scope.enemyMmr3 = bestMatch3.mmr;
+					$scope.enemyRegion3 = bestMatch3.region;
+					$scope.enemyGames3 = bestMatch3.numberOfGames;
+					$scope.enemyMatches3 = bestMatch3.numberOfMatches;
+					
+					if (!(angular.isUndefined($scope.enemyId3) || $scope.enemyId3 === null)) {
+						hhHistory.get({ id: $scope.enemyId3}, function(history) {
+							$log.info(history.rows);
+							$scope.matches3 = history.rows;
+						});
+					}
+				});
+				
+				var bestMatch4 = hhBestMatch.get({name: $scope.enemyName4}, function(bestMatch4) {
+					$log.info("Match4: " + bestMatch4.id);
+					
+					$scope.enemyId4 = bestMatch4.id;
+					$scope.enemyMmr4 = bestMatch4.mmr;
+					$scope.enemyRegion4 = bestMatch4.region;
+					$scope.enemyGames4 = bestMatch4.numberOfGames;
+					$scope.enemyMatches4 = bestMatch4.numberOfMatches;
+					
+					if (!(angular.isUndefined($scope.enemyId4) || $scope.enemyId4 === null)) {
+						hhHistory.get({ id: $scope.enemyId4}, function(history) {
+							$log.info(history.rows);
+							$scope.matches4 = history.rows;
+						});
+					}
+				});
+				
+				var bestMatch5 = hhBestMatch.get({name: $scope.enemyName5}, function(bestMatch5) {
+					$log.info("Match5: " + bestMatch5.id);
+					
+					$scope.enemyId5 = bestMatch5.id;
+					$scope.enemyMmr5 = bestMatch5.mmr;
+					$scope.enemyRegion5 = bestMatch5.region;
+					$scope.enemyGames5 = bestMatch5.numberOfGames;
+					$scope.enemyMatches5 = bestMatch5.numberOfMatches;
+					
+					if (!(angular.isUndefined($scope.enemyId5) || $scope.enemyId5 === null)) {
+						hhHistory.get({ id: $scope.enemyId5}, function(history) {
+							$log.info(history.rows);
+							$scope.matches5 = history.rows;
+						});
+					}
+				});
+				
+				$log.info('UPDATED!: ' + $scope.enemy1)
+				
+				
+				
+				$log.info('UPDATED2: ' + $scope.enemy2)
 			});
 		});
 	}
@@ -70,7 +176,7 @@ app.controller('hh_controller', function($scope, $log, hhHistory) {
 		
 		var history = hhHistory.get({ id: 4387231}, function(history) {
 			$log.info(history.rows);
-			$scope.matches = history.rows;
+			$scope.matches1 = history.rows;
 		});
 //		stompClient.disconnect();
 //		$scope.stompConnected = false;
